8f37b427f4c3533606280ff4801592543abe8811
Panson <PansonPanson@163.com>
2025-04-26 21:35:13 +0800

    新增核心模块 argo-core

---
+++ argo-core/src/main/java/top/panson/argocore/ArgoCoreApplication.java
@@ -0,0 +1,13 @@
+package top.panson.argocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgoCoreApplication.class, args);
+    }
+
+}
---
+++ argo-core/src/test/java/top/panson/argocore/ArgoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package top.panson.argocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgoCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
